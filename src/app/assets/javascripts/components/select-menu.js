3cdc289a1a200e01c1c7c8fa2a219273e7a24346
alexanderkozlov <alexanderk@coalla.ru>
2017-07-25 13:07:39 +0300

    manager edits + demo +3fix

--- src/app/assets/javascripts/components/select-menu.js
+++ src/app/assets/javascripts/components/select-menu.js
@@ -12,6 +12,21 @@ $(function () {
             $(document)
                 .find('.ui-selectmenu-menu.ui-selectmenu-open .ui-menu')
                 .css('width', $(event.currentTarget).width());
+        },
+
+        create: function(event, ui) {
+            const $select = $(event.target);
+
+            if (
+                $select
+                    .find('option:first-child')
+                    .is(':disabled')
+            ) {
+                $select
+                    .next('.ui-selectmenu-button')
+                    .find('.ui-selectmenu-text')
+                    .addClass('ui-state-placeholder');
+            }
         }
 
     });
