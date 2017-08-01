2d08da075f624253deef3ea645ee607467edfd19
alexanderkozlov <alexanderk@coalla.ru>
2017-08-01 20:41:04 +0300

    + universe

--- src/app/assets/javascripts/components/filter_slider.js
+++ src/app/assets/javascripts/components/filter_slider.js
@@ -20,6 +20,25 @@ $(function() {
         ]
     });
 
+    $('.js-filter-slider-universe').slick({
+        variableWidth: true,
+        slidesToShow: 7,
+        centerMode: true,
+        centerPadding: '0',
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: false,
+                    centerPadding: '65px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
 
     // On before slide change
     $('.js-filter-slider').on('init', function(event, slick, currentSlide, nextSlide){
@@ -35,11 +54,32 @@ $(function() {
 
     });
 
+    $('.js-filter-slider-universe a').on('click',function(){
+
+        $('.filter-slider__item')
+            .removeClass('is-all')
+            .removeClass('is-active');
+
+        $(this).addClass('is-active');
+
+    });
+
+    $('.js-filter-all').on('click',function(){
+
+        $('.filter-slider__item')
+            .removeClass('is-active')
+            .addClass('is-all');
+
+        $(this).addClass('is-active');
+
+    });
+
 
 
     $('.js-filter-slider-next').on('click',function(){
 
         $('.js-filter-slider').slick('slickNext');
+        $('.js-filter-slider-universe').slick('slickNext');
 
         return false;
     });
